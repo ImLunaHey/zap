97397e9bb3c000302430951115356716f3516ac3
luna <luna@wvvw.me>
2024-01-13 10:30:12 +1030

    fix: make sure routes log errors

--- src/app/api/push/route.ts
+++ src/app/api/push/route.ts
@@ -6,30 +6,42 @@ import { getSubscriptions, saveSubscription } from '@/app/redis';
 webpush.setVapidDetails('https://github.com/imlunahey/zap', PUBLIC_KEY, PRIVATE_KEY);
 
 export async function POST(request: Request) {
-  const subscription = (await request.json()) as PushSubscription | null;
+  try {
+    const subscription = (await request.json()) as PushSubscription | null;
 
-  if (!subscription) {
-    console.error('No subscription was provided!');
-    return;
+    console.log({ subscription });
+
+    if (!subscription) {
+      console.error('No subscription was provided!');
+      return;
+    }
+
+    await saveSubscription(subscription);
+
+    return Response.json({ message: 'success' });
+  } catch (error) {
+    console.error(error);
+    return Response.json({ message: 'error' });
   }
-
-  await saveSubscription(subscription);
-
-  return Response.json({ message: 'success' });
 }
 
 export async function GET() {
-  const subscriptions = await getSubscriptions();
+  try {
+    const subscriptions = await getSubscriptions();
 
-  for (const subscription of subscriptions) {
-    const payload = JSON.stringify({
-      title: 'WebPush Notification!',
-      body: 'Hello World',
+    for (const subscription of subscriptions) {
+      const payload = JSON.stringify({
+        title: 'WebPush Notification!',
+        body: 'Hello World',
+      });
+      webpush.sendNotification(subscription, payload);
+    }
+
+    return Response.json({
+      message: `${subscriptions.length} messages sent!`,
     });
-    webpush.sendNotification(subscription, payload);
+  } catch (error) {
+    console.error(error);
+    return Response.json({ message: 'error' });
   }
-
-  return Response.json({
-    message: `${subscriptions.length} messages sent!`,
-  });
 }
