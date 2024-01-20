de6193645ccaa2b3ba6c470c95470a93cc1d7bb6
luna <luna@wvvw.me>
2024-01-20 13:50:36 +1030

    fix: friends page

--- src/app/friends/page.tsx
+++ src/app/friends/page.tsx
@@ -1,5 +1,4 @@
-import { getFriends } from '../../redis';
-import { Subscription } from './Subscription';
+import { Friends } from '@/components/friends';
 
 export default async function Page(props: {
   searchParams: {
@@ -11,14 +10,5 @@ export default async function Page(props: {
   // Redirect to home page if no ID is provided
   if (!id) return Response.redirect('/');
 
-  const friends = await getFriends(id);
-
-  return (
-    <div>
-      <h1>Friends</h1>
-      {friends.map((subscription) => {
-        return <Subscription key={subscription.id} {...subscription} />;
-      })}
-    </div>
-  );
+  return <Friends />;
 }
